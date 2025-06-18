a16c3f2c7c6a5ac84e55288b81e2205b391ce259
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-06-18 12:11:42 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,12 @@
+public class  Unary  {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("\nUnary Operators:");
+        int d = 10;
+        System.out.println("d = " + d);
+        System.out.println("++d = " + (++d));
+        System.out.println("--d = " + (--d));
+        System.out.println("-d = " + (-d));
+    }
+}
