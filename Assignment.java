2bf4d63e2decb344afac3e86265463f244e54c73
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-06-18 12:09:55 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,19 @@
+public class Assignment  {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("\nAssignment Operators:");
+        int c = 15;
+        System.out.println("Initial c: " + c);
+        c += 5;
+        System.out.println("c += 5: " + c);
+        c -= 3;
+        System.out.println("c -= 3: " + c);
+        c *= 2;
+        System.out.println("c *= 2: " + c);
+        c /= 4;
+        System.out.println("c /= 4: " + c);
+        c %= 3;
+        System.out.println("c %= 3: " + c);
+
+        }
+}
