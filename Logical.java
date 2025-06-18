7b427e89a902361464ade32acd22cf4ac522619b
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-06-18 12:10:56 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,12 @@
+public class Logical {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        boolean x = true, y = false;
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+}
+}
+
+       
