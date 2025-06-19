a875b4e7ecf594650c298bba2e0ac62e4dd6cede
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-06-19 11:53:42 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,60 @@
+/*class Methods
+{
+public void add()
+{
+System.out.println(10+20);
+}
+public static void main(String[] args)
+{
+Methods m=new Methods();
+m.add();
+}
+}*/
+
+
+/*class Methods
+{
+public int sub()
+{
+ int a=10-5;
+return a;
+}
+public static void main(String[] args)
+{
+Methods m=new Methods();
+ int b=m.sub();
+System.out.println(b);
+}
+}*/
+ 
+
+/*class Methods
+{
+public void mult(int a,int b)
+{
+System.out.println(a*b);
+}
+public static void main(String[] args)
+{
+Methods m=new Methods();
+m.mult(5,2);
+}
+}*/
+
+
+class Method
+{
+public int add(int a,int b)
+{
+	return(a+b);
+}
+public static void main (String [] args)
+{
+Method m=new Method();
+int c;
+c=m.add(10,11);
+System.out.print(c);
+}
+}
+
+
